2ab51c1b3a3a4205e077923755c4531297dc95cb
Elisa Sanchez <elisasanchez@MacBook-Pro.local>
2020-09-27 17:25:23 -0400

    Adding Deliverable 1

--- static/js/app.js
+++ static/js/app.js
@@ -33,26 +33,22 @@ function updateFilters() {
     let changedElement = d3.select(this);
     // 4b. Save the value that was changed as a variable.
     var elementValue = changedElement.property("value");
-    console.log(elementValue);
     // 4c. Save the id of the filter that was changed as a variable.
     var filterId = changedElement.attr('id');
-    console.log(filterId);
   
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
     if (elementValue) {
         filters[filterId] = elementValue; 
-        console.log(filters[filterId]);
     } else {
         delete filters[filterId];
     };
 
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
-
-    console.log(filters);
-  
   };
+
+  console.log(filters);
   
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
@@ -65,8 +61,6 @@ function updateFilters() {
 
     for (var key in filters) {
         filteredData = filteredData.filter(row => row.key === filters[key]);
- 
-        console.log(filteredData);
     };
 
     // 10. Finally, rebuild the table using the filtered data
